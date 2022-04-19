3806699d3879d50522c35be4dfe7bfaf39711fe5
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-04-19 15:46:50 +0300

    picking photo from device

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -9,16 +9,38 @@ import UIKit
 import CoreML
 import Vision
 
-class ViewController: UIViewController, UIPickerViewDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+        }
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CodeML Model failed.")
+        }
+    }
 
     @IBAction func CameraTapped(_ sender: UIBarButtonItem) {
-        print("camera tapped")
+        present(imagePicker, animated: true, completion: nil)
+        
     }
 }
 
